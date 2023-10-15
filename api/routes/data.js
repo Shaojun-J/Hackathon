a3cc4a01d8224e19bbaec90987f761a9aff4b8b3
Shaojun-J <sjmyreg@outlook.com>
2023-10-15 10:19:45 -0400

    modify api/routes/data.js for mock data

--- api/routes/data.js
+++ api/routes/data.js
@@ -6,8 +6,19 @@ const reports = require('../mock_data/reports.json')
 router.post("/", (req, res) => {
     const receivedData = req.body;   
     //console.log(receivedData);
-    let index = Math.floor(Math.random() * reports.length);
-    const report = reports[index];
+    let  report =reports[0];
+    if (receivedData.post.search("Chuck E. Cheese's") > -1){
+        report = reports[0];
+    }
+    else if(receivedData.post.search("XYZ Corporation") > -1){
+         report = reports[1];
+    }
+    else if(receivedData.post.search("const report = reports[0];") > -1){
+        report = reports[2];
+    }
+    else {
+        report = reports[Math.floor(Math.random() * reports.length)];
+    }
     res.json(
         {
             message: "Data received!",
